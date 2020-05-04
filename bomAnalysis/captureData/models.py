22ad9cc0528f8ace4e1ded00c5f5f4909942be1c
Prerna <prerna1998.ps@gmail.com>
2020-05-04 21:44:18 +0530

    Model design finalised

--- bomAnalysis/captureData/models.py
+++ bomAnalysis/captureData/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class data(models.Model):
 
-    pub_date=models.DateField(auto_now=True)
     pub_time=models.DateTimeField(auto_now=True)
+
     pressure_avg=models.FloatField()
     pressure_min=models.FloatField()
     pressure_max=models.FloatField()
@@ -13,5 +14,12 @@ class data(models.Model):
     wind_speed_avg=models.FloatField()
     wind_speed_min=models.FloatField()
     wind_speed_max=models.FloatField()
+
+    def __str__(self):
+        record='Time -> {}  |  Pressure -> Avg. : {} | Min. : {} | Max. : {}  Wind Speed -> Avg. : {} | Min. : {} | Max. : {} '.format(str(self.pub_time),str(self.pressure_avg), str(self.pressure_min), str(self.pressure_max), str(self.wind_speed_avg), str(self.wind_speed_min), str(self.wind_speed_max))
+        return record
+
+#constructor
+#=data(pressure_avg=,pressure_min=,pressure_max=,wind_speed_avg=,wind_speed_min=,wind_speed_max
 
 
