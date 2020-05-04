3a0c672cc2457a8212b44b1cb7296b9ff9f9c537
Prerna <prerna1998.ps@gmail.com>
2020-05-04 22:03:33 +0530

    History View Defined | All Samples View

--- bomAnalysis/captureData/views.py
+++ bomAnalysis/captureData/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import data
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("Have a good day!")
+
+def history(request):
+    allSamples=data.objects.all()
+    return HttpResponse(allSamples)
