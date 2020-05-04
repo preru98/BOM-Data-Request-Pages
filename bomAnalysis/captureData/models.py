cc544c6ebabf5e376c365bcb0fe6a23999ccd2f6
Prerna <prerna1998.ps@gmail.com>
2020-05-04 12:58:37 +0530

    Testing first model design

--- bomAnalysis/captureData/models.py
+++ bomAnalysis/captureData/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class data(models.Model):
+
+    pub_date=models.DateField(auto_now=True)
+    pub_time=models.DateTimeField(auto_now=True)
+    pressure_avg=models.FloatField()
+    pressure_min=models.FloatField()
+    pressure_max=models.FloatField()
+
+    wind_speed_avg=models.FloatField()
+    wind_speed_min=models.FloatField()
+    wind_speed_max=models.FloatField()
+
+
