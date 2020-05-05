3c1658a572a2896d63abac20626c5b5072fb6ab6
Prerna <prerna1998.ps@gmail.com>
2020-05-05 17:25:50 +0530

    Created all views

--- bomAnalysis/captureData/urls.py
+++ bomAnalysis/captureData/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('history/',views.history,name='history'),
+    path('analysis/',views.analysis,name='analysis'),
+    path('recent/<int:duration>/days',views.recent_day,name='recent_day'),
+    path('recent/<int:duration>/hours',views.recent_hour,name='recent_hour'),
+    path('recent/<int:duration>/weeks',views.recent_week,name='recent_week'),
 ]
--- bomAnalysis/captureData/views.py
+++ bomAnalysis/captureData/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . models import data
+from django.utils import timezone
+from datetime import datetime,timedelta
 
 # Create your views here.
 
@@ -8,5 +10,32 @@ def index(request):
     return HttpResponse("Have a good day!")
 
 def history(request):
-    allSamples=data.objects.all()
+    allSamples=data.objects.all().order_by('-pub_time')
     return HttpResponse(allSamples)
+
+#One day only
+def analysis(request):
+    sampling_date = datetime.now() - timedelta(days=1)
+    return request_interval(sampling_date)
+    
+#past<duration>days
+def recent_day(request,duration):
+    sampling_date = datetime.now() - timedelta(days=duration)
+    return request_interval(sampling_date)
+
+#past<duration>hours
+def recent_hour(request,duration):
+    sampling_date = datetime.now() - timedelta(hours=duration)
+    return request_interval(sampling_date)
+    
+#past<duration>weeks
+def recent_week(request,duration):
+    sampling_date = datetime.now() - timedelta(weeks=duration)
+    return request_interval(sampling_date)
+
+def request_interval(sampling_date):
+    today=datetime.now()
+    present_day_samples=data.objects.filter(pub_time__range=(sampling_date ,today)).order_by('-pub_time')
+    return HttpResponse(present_day_samples)
+
+
